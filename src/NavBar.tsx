91909ed7aed14c10ed514387c70a69cdc5350585
mycahdt <mycahdt@udel.edu>
2022-10-18 14:22:28 -0400

    home page as default

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,51 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
+import { HomePage } from "./pages/HomePage";
 
 export function NavBar() {
+    const [visible, setVisible] = useState<boolean>(true);
     const activeStyle = {
         textDecoration: "underline",
         color: "black"
     };
     return (
-        <nav className="navbar-container">
-            <NavLink
-                end
-                to="/"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-                Home
-            </NavLink>
-            <NavLink
-                to="/about"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-                About
-            </NavLink>
-            <NavLink
-                to="/contact"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-                Contact Us
-            </NavLink>
-            <NavLink
-                to="/join"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-                Join
-            </NavLink>
-            <NavLink
-                to="/past"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-                Past Events
-            </NavLink>
-            <NavLink
-                to="/future"
-                style={({ isActive }) => (isActive ? activeStyle : undefined)}
-            >
-                Future Events
-            </NavLink>
-        </nav>
+        <div>
+            <nav className="navbar-container">
+                <NavLink
+                    end
+                    to="/"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                    onClick={() => setVisible(false)}
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                    onClick={() => setVisible(false)}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/contact"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                    onClick={() => setVisible(false)}
+                >
+                    Contact Us
+                </NavLink>
+                <NavLink
+                    to="/join"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                    onClick={() => setVisible(false)}
+                >
+                    Join
+                </NavLink>
+                <NavLink
+                    to="/past"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                    onClick={() => setVisible(false)}
+                >
+                    Past Events
+                </NavLink>
+                <NavLink
+                    to="/future"
+                    style={({ isActive }) =>
+                        isActive ? activeStyle : undefined
+                    }
+                    onClick={() => setVisible(false)}
+                >
+                    Future Events
+                </NavLink>
+            </nav>
+            {visible && <HomePage />}
+        </div>
     );
 }
