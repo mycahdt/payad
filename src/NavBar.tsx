4d66c654b06ad718063738e95dc09dfa309467b7
mycahdt <mycahdt@udel.edu>
2022-11-01 20:36:43 -0400

    navbar styling

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Col } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import { HomePage } from "./pages/HomePage";
@@ -16,61 +17,66 @@ export function NavBar() {
     return (
         <div>
             <nav className="navbar-container">
-                <NavLink
-                    end
-                    to="/"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : notActiveStyle
-                    }
-                    onClick={() => setVisible(false)}
-                >
-                    Home
-                </NavLink>
-                <NavLink
-                    to="/about"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : notActiveStyle
-                    }
-                    onClick={() => setVisible(false)}
-                >
-                    About
-                </NavLink>
-                <NavLink
-                    to="/contact"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : notActiveStyle
-                    }
-                    onClick={() => setVisible(false)}
-                >
-                    Contact Us
-                </NavLink>
-                <NavLink
-                    to="/join"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : notActiveStyle
-                    }
-                    onClick={() => setVisible(false)}
-                >
-                    Join
-                </NavLink>
-                <NavLink
-                    to="/past"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : notActiveStyle
-                    }
-                    onClick={() => setVisible(false)}
-                >
-                    Past Events
-                </NavLink>
-                <NavLink
-                    to="/future"
-                    style={({ isActive }) =>
-                        isActive ? activeStyle : notActiveStyle
-                    }
-                    onClick={() => setVisible(false)}
-                >
-                    Future Events
-                </NavLink>
+                <Col className="left-column" sm={4}>
+                    PAYAD
+                </Col>
+                <Col className="right-column" sm={8}>
+                    <NavLink
+                        end
+                        to="/"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : notActiveStyle
+                        }
+                        onClick={() => setVisible(false)}
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : notActiveStyle
+                        }
+                        onClick={() => setVisible(false)}
+                    >
+                        About
+                    </NavLink>
+                    <NavLink
+                        to="/contact"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : notActiveStyle
+                        }
+                        onClick={() => setVisible(false)}
+                    >
+                        Contact Us
+                    </NavLink>
+                    <NavLink
+                        to="/join"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : notActiveStyle
+                        }
+                        onClick={() => setVisible(false)}
+                    >
+                        Join
+                    </NavLink>
+                    <NavLink
+                        to="/past"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : notActiveStyle
+                        }
+                        onClick={() => setVisible(false)}
+                    >
+                        Past Events
+                    </NavLink>
+                    <NavLink
+                        to="/future"
+                        style={({ isActive }) =>
+                            isActive ? activeStyle : notActiveStyle
+                        }
+                        onClick={() => setVisible(false)}
+                    >
+                        Future Events
+                    </NavLink>
+                </Col>
             </nav>
             {visible && <HomePage />}
         </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,10 @@ export function HomePage(): JSX.Element {
     return (
         <div>
             <h1>Home Page</h1>
+            <img
+                src="/Users/mycahdetorres/Documents/SideProjects/Project_Payad/payad/src/images/payadLogo.jpg"
+                alt="A picture of the PAYAD logo"
+            />
         </div>
     );
 }
