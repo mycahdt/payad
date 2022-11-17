2a589ac7a6b78141578081530bdc56476dc6a099
mycahdt <mycahdt@udel.edu>
2022-11-17 13:18:03 -0500

    added google form

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -4,6 +4,11 @@ export function ContactPage(): JSX.Element {
     return (
         <div>
             <h1>Contact Page</h1>
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSdl7-S9KQlaQmKC5jpcJpN1WJ1UcTt2p6u3PUCgsOAmtq4yog/viewform"
+                width="60%"
+                height="2250"
+            ></iframe>
         </div>
     );
 }
